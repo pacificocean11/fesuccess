58cdaffc499c6964a62924217726e7f83d51757f
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2021-12-03 19:11:04 +0530

    add allowed host

--- myenggtv/settings.py
+++ myenggtv/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fesuccess.herokuapp.com']
 
 
 # Application definition
@@ -142,3 +142,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
