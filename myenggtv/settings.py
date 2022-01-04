5bcb3ce6a62de8e1b757b9921a5417b994bd72db
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2022-01-04 11:05:42 +0530

    debug settings

--- myenggtv/settings.py
+++ myenggtv/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+# DEBUG = False
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['fesuccess.herokuapp.com', '*', 'fesuccess.com']
 
